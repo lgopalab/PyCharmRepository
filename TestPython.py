8a7a705398dcf0dfa0c878cb00b78253c12bbe14
Mahi <lgopalab@uncc.edu>
2016-02-26 13:08:57 -0500

    PyCharm PuSh

--- TestPython.py
+++ TestPython.py
@@ -1,2 +1,42 @@
+import mysql.connector
+from mysql.connector import errorcode
+
+
 print("Namaste Macha!")
 print("endha chicha!")
+from flask import Flask
+"""
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    app.run()
+"""
+
+
+
+try:
+  cnx = mysql.connector.connect(user='root',password='123',
+                                database='TEST')
+except mysql.connector.Error as err:
+  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+    print("Something is wrong with your user name or password")
+  elif err.errno == errorcode.ER_BAD_DB_ERROR:
+    print("Database does not exist")
+  else:
+    print(err)
+else:
+
+  cursor = cnx.cursor()
+  query = ("select sno,name from TEST");
+  cursor.execute(query)
+
+  for (sno, name) in cursor:
+    print("{}, With Serial umber {}".format(name,sno))
+
+  cursor.close()
+  cnx.close()
+  cnx.close()
